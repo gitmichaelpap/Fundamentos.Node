fad7eb0c093eb710e7f305a3f85bec95e961c1cb
Michael Pacheco <michaelpacheco.br@gmail.com>
2025-01-20 13:56:01 -0300

    aula 1

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import http from 'node:http';
+
+// Create an HTTP server
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+});
+
+server.listen(3333);
