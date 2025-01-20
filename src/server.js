472368e09f214e0ccb81b7a3da43ed54419f1421
Michael Pacheco <michaelpacheco.br@gmail.com>
2025-01-20 14:06:54 -0300

    aula 2

--- src/server.js
+++ src/server.js
@@ -3,8 +3,16 @@ import http from 'node:http';
 // Create an HTTP server
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+ const { url, method } = req;
+
+ if (method === 'GET' && url === '/users') {
+    return res.end('Listagem de usuários')
+  }
+  if (method === 'POST' && url === '/users') {
+    return res.end('Criação de usuário')
+  }
+
+  return res.end('Hello World\n');
 });
 
 server.listen(3333);
