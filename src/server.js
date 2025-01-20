02a9d6ed458a5b13c15a4e5ab187a8143e23c6c2
Michael Pacheco <michaelpacheco.br@gmail.com>
2025-01-20 14:30:21 -0300

    aula 3

--- src/server.js
+++ src/server.js
@@ -1,18 +1,29 @@
-import http from 'node:http';
+import http from "node:http";
 
 // Create an HTTP server
 
+const users = [];
+
 const server = http.createServer((req, res) => {
- const { url, method } = req;
+  const { url, method } = req;
 
- if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários')
-  }
-  if (method === 'POST' && url === '/users') {
-    return res.end('Criação de usuário')
+  if (method === "GET" && url === "/users") {
+    return res
+      .writeHead(200, { "Content-Type": "application/json" })
+      .end(JSON.stringify(users));
   }
 
-  return res.end('Hello World\n');
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: users.length + 1,
+      name: "John Doe",
+      email: "johndoe@example.com",
+    });
+
+    return res.writeHead(201).end();
+  }
+
+  return res.writeHead(404).end();
 });
 
 server.listen(3333);
