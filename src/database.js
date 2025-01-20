58a2a14bb9200cf61db991af11c3d0e4bf33f043
Michael Pacheco <michaelpacheco.br@gmail.com>
2025-01-20 17:48:35 -0300

    aula 10
    aula 10

---
+++ src/database.js
@@ -0,0 +1,17 @@
+export class Database {
+  #database = {};
+
+  select(table) {
+    return this.database[table] ?? [];
+  }
+
+  insert(table, data) {
+    if (!this.database[table]) {
+      this.database[table] = [];
+    }
+
+    this.database[table].push(data);
+
+    return data;
+  }
+}
