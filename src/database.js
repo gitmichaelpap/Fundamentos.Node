cbaddd7ce198a2089520d4be5b6477f6137143c1
Michael Pacheco <michaelpacheco.br@gmail.com>
2025-01-20 18:07:12 -0300

    aula 11

--- src/database.js
+++ src/database.js
@@ -1,16 +1,35 @@
+import fs from 'node:fs/promises';
+
+const databasepath = new URL('../db.json', import.meta.url);
 export class Database {
   #database = {};
 
+  constructor() {
+    fs.readFile(databasepath)
+      .then((data) => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#database = {};
+      });
+  }
+
+  #persist() {
+    fs.writeFile(databasepath, JSON.stringify(this.#database));
+  }
+
   select(table) {
-    return this.database[table] ?? [];
+    return this.#database[table] ?? [];
   }
 
   insert(table, data) {
-    if (!this.database[table]) {
-      this.database[table] = [];
+    if (!this.#database[table]) {
+      this.#database[table] = [];
     }
 
-    this.database[table].push(data);
+    this.#database[table].push(data);
+
+    this.#persist();
 
     return data;
   }
