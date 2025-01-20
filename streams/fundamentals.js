fcacc3e4d12003701f9b3bac8ab6869e0385ca8e
Michael Pacheco <michaelpacheco.br@gmail.com>
2025-01-20 16:05:36 -0300

    aula 5

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,21 +1,44 @@
 // process.stdin.pipe(process.stdout);
 
-import { Readable, Writable } from 'node:stream';
+import { Readable, Writable, Transform } from "node:stream";
 
 class OneToHundred extends Readable {
   i = 1;
 
   _read() {
-    const index = this.i++;
+    setTimeout(() => {
+      const index = this.i++;
 
-    if (index < 100) {
+      if (index < 100) {
         const buffer = Buffer.from(String(index));
 
-      this.push(buffer);
-    } else {
-      this.push(null);
-    }
+        this.push(buffer);
+      } else {
+        this.push(null);
+      }
+    }, 1000);
   }
 }
 
-new OneToHundred().pipe(process.stdout);
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const number = parseInt(chunk.toString());
+    const result = number * -1;
+
+    callback(null, Buffer.from(result.toString()));
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    const number = parseInt(chunk.toString());
+    const result = number * 10;
+
+    console.log(result);
+    callback();
+  }
+}
+
+new OneToHundred()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream());
